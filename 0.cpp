16d40e94ec38b1cacbe516eb872ef936d890583c
Dmitry Panin <pank.dm@gmail.com>
2012-04-16 20:34:04 +0400

    Template for C++ programs

---
+++ 0.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <bitset>
+#include <set>
+#include <sstream>
+#include <stdlib.h>
+#include <map>
+#include <queue>
+#include <assert.h>
+#include <deque>
+#include <string.h>
+
+
+using namespace std;
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+typedef vector <int> vi;
+typedef vector <vi> vvi;
+
+
+
+
+
+int main () {
+	//freopen("", "rt", stdin);
+	//freopen("", "wt", stdout);
+
+
+    return 0;
+}
+
